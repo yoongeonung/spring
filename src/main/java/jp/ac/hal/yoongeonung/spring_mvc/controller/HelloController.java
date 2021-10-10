a7d59735e191acca95433dba01faa1e3d92465de
yoongeonung <yoongeonung@gmail.com>
2021-10-10 18:21:41 +0900

    feat : Add view template controller & use request param annotation

--- src/main/java/jp/ac/hal/yoongeonung/spring_mvc/controller/HelloController.java
+++ src/main/java/jp/ac/hal/yoongeonung/spring_mvc/controller/HelloController.java
@@ -3,13 +3,21 @@ package jp.ac.hal.yoongeonung.spring_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
+    // template
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
     }
+    // template, use request param
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
