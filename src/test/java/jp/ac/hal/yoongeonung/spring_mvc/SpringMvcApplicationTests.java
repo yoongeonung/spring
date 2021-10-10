db1f02d21c5b89cbc1416bb75753aecdd806dca1
yoongeonung <yoongeonung@gmail.com>
2021-10-10 17:34:56 +0900

    Project Setup

---
+++ src/test/java/jp/ac/hal/yoongeonung/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hal.yoongeonung.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
