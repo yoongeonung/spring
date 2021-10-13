834657053dff73c3adcdef227a963118910a821d
yoongeonung <yoongeonung@gmail.com>
2021-10-13 13:23:32 +0900

    feat : Add create member & members list

---
+++ src/main/java/jp/ac/hal/yoongeonung/spring_mvc/controller/MemberForm.java
@@ -0,0 +1,10 @@
+package jp.ac.hal.yoongeonung.spring_mvc.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+    // dto ? vo?
+    private String name;
+}
