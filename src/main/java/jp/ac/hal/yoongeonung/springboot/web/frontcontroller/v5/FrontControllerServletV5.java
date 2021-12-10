8c2ffa76a9e0ae92da4232a010bf8ebd63da0693
yoongeonung <yoongeonung@gmail.com>
2021-12-10 23:22:44 +0900

    feat : Front Controller V5 - V3

---
+++ src/main/java/jp/ac/hal/yoongeonung/springboot/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,71 @@
+package jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v5;
+
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.ModelView;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.MyView;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v3.ControllerV3;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v3.controller.MemberListControllerV3;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        Object handler = handlerMappingMap.get(uri);
+        if (handler == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+        ModelView mv = adapter.handle(req, resp, handler);
+
+        MyView view = viewResolver(mv);
+        view.render(mv.getModel(), req, resp);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("Handler Adapter를 찾을수 없습니다." + handler);
+    }
+
+    private MyView viewResolver(ModelView mv) {
+        return new MyView("/WEB-INF/views/" + mv.getViewName() + ".jsp");
+    }
+}
---
+++ src/main/java/jp/ac/hal/yoongeonung/springboot/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,14 @@
+package jp.ac.hal.yoongeonung.springboot.web.frontcontroller.v5;
+
+import jp.ac.hal.yoongeonung.springboot.web.frontcontroller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
