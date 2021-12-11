827edf1bafde7f6c1732ec854897647aa20d8efb
yoongeonung <yoongeonung@gmail.com>
2021-12-11 09:26:34 +0900

    feat : Spring MVC V1

---
+++ src/main/java/jp/ac/hal/yoongeonung/springboot/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,24 @@
+package jp.ac.hal.yoongeonung.springboot.web.springmvc.v1;
+
+import jp.ac.hal.yoongeonung.springboot.domain.member.Member;
+import jp.ac.hal.yoongeonung.springboot.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
