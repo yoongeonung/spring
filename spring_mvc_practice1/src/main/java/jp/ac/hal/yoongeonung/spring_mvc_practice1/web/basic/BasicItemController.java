c9bccc079d060e44233353fb45f8cd96c2f92a2e
yoongeonung <yoongeonung@gmail.com>
2021-12-23 22:48:50 +0900

    feat : RedirectAttributes

--- spring_mvc_practice1/src/main/java/jp/ac/hal/yoongeonung/spring_mvc_practice1/web/basic/BasicItemController.java
+++ spring_mvc_practice1/src/main/java/jp/ac/hal/yoongeonung/spring_mvc_practice1/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -64,11 +65,24 @@ public class BasicItemController {
         return "redirect:/basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String saveV4(@ModelAttribute Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
     }
+
+    @PostMapping("/add")
+    public String saveV5(@ModelAttribute Item item,
+                         RedirectAttributes redirectAttributes
+    ) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        // 추가하려는 속성이 return문 안에 없을시 자동적으로 경로변수(path variable) 형식으로 추가한다 -> ?status=true
+        // 또한 URL 인코딩또한 해준다.
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
