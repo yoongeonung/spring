e202e0810cc0fc45eaaabbc82e598042de81ffdb
yoongeonung <yoongeonung@gmail.com>
2021-12-28 21:56:00 +0900

    feat : Thymeleaf block

--- spring_mvc_02/thymeleaf_basic/src/main/java/jp/ac/hal/yoongeonung/thymeleaf_basic/basic/BasicController.java
+++ spring_mvc_02/thymeleaf_basic/src/main/java/jp/ac/hal/yoongeonung/thymeleaf_basic/basic/BasicController.java
@@ -105,6 +105,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
 //    @Component
 //    static class HelloBean {
 //        public String hello(String data) {
