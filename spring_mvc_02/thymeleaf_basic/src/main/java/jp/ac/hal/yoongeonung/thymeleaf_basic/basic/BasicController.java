2da17a194bd2a26b3c53a32d2337b1fca42009ca
yoongeonung <yoongeonung@gmail.com>
2021-12-29 00:37:52 +0900

    feat : Thymeleaf javascript

--- spring_mvc_02/thymeleaf_basic/src/main/java/jp/ac/hal/yoongeonung/thymeleaf_basic/basic/BasicController.java
+++ spring_mvc_02/thymeleaf_basic/src/main/java/jp/ac/hal/yoongeonung/thymeleaf_basic/basic/BasicController.java
@@ -111,6 +111,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
 //    @Component
 //    static class HelloBean {
 //        public String hello(String data) {
