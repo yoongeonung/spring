9e17b8175f0ba2eaf3de6fdd1f37a18e60b8a87f
yoongeonung <yoongeonung@gmail.com>
2022-01-24 20:37:18 +0900

    feat : Stateful & Stateless Singleton Bean

---
+++ springbasic/src/test/java/yoongeonung/springbasic/singleton/StatelessService.java
@@ -0,0 +1,9 @@
+package yoongeonung.springbasic.singleton;
+
+public class StatelessService {
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+}
