29e221b91e1dda35b65566a4b2fcc343a736705a
yoongeonung <yoongeonung@gmail.com>
2022-02-04 22:55:37 +0900

    feat : InitializingBean & DisposableBean

--- springbasic/src/test/java/yoongeonung/springbasic/lifecycle/NetworkClient.java
+++ springbasic/src/test/java/yoongeonung/springbasic/lifecycle/NetworkClient.java
@@ -1,15 +1,15 @@
 package yoongeonung.springbasic.lifecycle;
 
 import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
     @Setter
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void call(String message) {
@@ -22,5 +22,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
     }
 }
