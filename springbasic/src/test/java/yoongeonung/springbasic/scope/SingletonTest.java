6365c16010e8514b7cdba23534b08af169184a8a
yoongeonung <yoongeonung@gmail.com>
2022-02-05 09:00:48 +0900

    feat : Singleton scope

---
+++ springbasic/src/test/java/yoongeonung/springbasic/scope/SingletonTest.java
@@ -0,0 +1,40 @@
+package yoongeonung.springbasic.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("싱글톤 스코프 테스트")
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        // == 비교
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close");
+        }
+    }
+}
