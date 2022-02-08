ccb33a6fd6ea77b15a838d4421725950bbe08045
yoongeonung <yoongeonung@gmail.com>
2022-02-08 10:13:54 +0900

    feat : Prototype bean with singleton

--- springbasic/src/test/java/yoongeonung/springbasic/scope/PrototypeProviderTest.java
+++ springbasic/src/test/java/yoongeonung/springbasic/scope/PrototypeProviderTest.java
@@ -2,47 +2,45 @@ package yoongeonung.springbasic.scope;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
-    @Test
-    @DisplayName("프로토타입 빈 직접 조회")
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
-        bean1.addCount();
-        assertThat(bean1.getCount()).isEqualTo(1);
-        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
-        bean2.addCount();
-        assertThat(bean2.getCount()).isEqualTo(1);
-    }
+public class PrototypeProviderTest {
 
     @Test
-    @DisplayName("싱글톤 빈에서 프로토타입빈을 함께 사용시")
-    void singletonClientUsePrototype() {
+    @DisplayName("필요시마다 컨테이너에 요청")
+    void providerTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
         ClientBean bean1 = ac.getBean(ClientBean.class);
-        int count1 = bean1.logic();
-        assertThat(count1).isEqualTo(1);
+        int count = bean1.logic();
+        assertThat(count).isEqualTo(1);
+
         ClientBean bean2 = ac.getBean(ClientBean.class);
         int count2 = bean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
+
     }
 
     @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+
+//        private final ApplicationContext ac;
+        private final ObjectProvider<PrototypeBean> provider;
+
 
         public int logic() {
+            // 필요시마다 컨테이너에 직접 요청 -> 이런 방법을 Dependency Lookup (DL) 이라고 한다.
+//            PrototypeBean bean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = provider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
