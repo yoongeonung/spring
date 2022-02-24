cb3f6f82ecf8b969ab131ac3482ec3fb1bfb01d2
yoongeonung <yoongeonung@gmail.com>
2022-02-24 13:53:00 +0900

    feat : Adjust Interface

--- tobyspring/dao/src/main/java/dao/UserDao.java
+++ tobyspring/dao/src/main/java/dao/UserDao.java
@@ -6,17 +6,23 @@ import java.sql.*;
 
 public class UserDao {
 
-    private static SimpleConnectionMaker simpleConnectionMaker;
+    //    private static SimpleConnectionMaker simpleConnectionMaker;
+    // 인터페이스를 의존하도록 변경, 추상에 의존
+    private static ConnectionMaker connectionMaker;
+
 
     public UserDao() {
-        simpleConnectionMaker = new SimpleConnectionMaker();
+//        simpleConnectionMaker = new SimpleConnectionMaker();
+        // 하지만 여기서 new해서 구체클래스에 의존하고있다.
+        connectionMaker = new DConnectionMaker();
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         // 중복 제거, 메소드로 추출
 //        Connection c = getConnection();
         // 추상클래스를 제거, 클래스로 분리후 참조
-        Connection c = simpleConnectionMaker.makeNewConnection();
+//        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection(); // 인터페이스(추상)에 의존
         PreparedStatement ps = c.prepareStatement( "insert into user(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -30,7 +36,8 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException{
 //        Connection c = getConnection();
-        Connection c = simpleConnectionMaker.makeNewConnection();
+//        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from user where id = ?");
         ps.setString(1, id);
 
