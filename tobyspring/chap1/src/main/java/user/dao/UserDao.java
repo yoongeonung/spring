9da02fb5b3a23bea5da297ab5398d38a5e154606
yoongeonung <yoongeonung@gmail.com>
2022-03-01 14:17:11 +0900

    refact : UserDao - getConnection()

--- tobyspring/chap1/src/main/java/user/dao/UserDao.java
+++ tobyspring/chap1/src/main/java/user/dao/UserDao.java
@@ -6,8 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspring", "root", "00000000");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into USER (id, name, password) values (?,?,?)");
         ps.setString(1, user.getId());
@@ -21,8 +20,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspring", "root", "00000000");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from USER where id = ?");
         ps.setString(1, id);
@@ -39,6 +37,11 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/tobyspring", "root", "00000000");
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
