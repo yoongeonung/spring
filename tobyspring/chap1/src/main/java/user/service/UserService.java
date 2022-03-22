49525f63e95f2e6b7daf0f4b1cce9dfa251d4e20
yoongeonung <yoongeonung@gmail.com>
2022-03-22 09:20:23 +0900

    feat : UserService.upgradeLevels()

--- tobyspring/chap1/src/main/java/user/service/UserService.java
+++ tobyspring/chap1/src/main/java/user/service/UserService.java
@@ -1,6 +1,10 @@
 package user.service;
 
 import user.dao.UserDao;
+import user.domain.Level;
+import user.domain.User;
+
+import java.util.List;
 
 public class UserService {
 
@@ -8,5 +12,26 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevel() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            Boolean changed = null;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
--- tobyspring/chap1/src/test/java/user/service/UserServiceTest.java
+++ tobyspring/chap1/src/test/java/user/service/UserServiceTest.java
@@ -1,7 +1,61 @@
 package user.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import user.dao.UserDao;
+import user.domain.Level;
+import user.domain.User;
 
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(locations = "/applicationContext.xml")
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserDao dao;
+    private List<User> users; // fixtrue
+
+    @BeforeEach
+    void beforeEach() {
+        users = Arrays.asList(
+                new User("1", "USER1", "1111", Level.BASIC, 49, 0),
+                new User("2", "USER2", "2222", Level.BASIC, 50, 0),
+                new User("3", "USER3", "3333", Level.SILVER, 60, 29),
+                new User("4", "USER4", "4444", Level.SILVER, 60, 30),
+                new User("5", "USER5", "5555", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    void upgradeLevels() {
+        dao.deleteAll();
+        Assertions.assertThat(dao.getAll().size()).isEqualTo(0);
+        for (User user : users) {
+            dao.add(user);
+        }
+        Assertions.assertThat(dao.getAll().size()).isEqualTo(5);
+
+        userService.upgradeLevel();
+
+        List<User> savedUsers = dao.getAll();
+        checkLevel(savedUsers.get(0), Level.BASIC);
+        checkLevel(savedUsers.get(1), Level.SILVER);
+        checkLevel(savedUsers.get(2), Level.SILVER);
+        checkLevel(savedUsers.get(3), Level.GOLD);
+        checkLevel(savedUsers.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        Assertions.assertThat(user.getLevel()).isEqualTo(expectedLevel);
+    }
 
 }
