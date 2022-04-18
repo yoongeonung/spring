1e9ac5a4f9683c2bed7c5c2d0d74e16974b6ad89
yoongeonung <yoongeonung@gmail.com>
2022-04-18 11:15:16 +0900

    feat : DI for additional features

---
+++ tobyspring/src/main/java/user/dao/CountingConnectionMaker.java
@@ -0,0 +1,23 @@
+package user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+    int counter = 0;
+    private final ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        counter++;
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
--- tobyspring/src/main/java/user/dao/UserDaoTest.java
+++ tobyspring/src/main/java/user/dao/UserDaoTest.java
@@ -2,7 +2,6 @@ package user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import user.domain.User;
 
 import java.sql.SQLException;
 
@@ -10,21 +9,34 @@ public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
+//        UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("1");
-        user.setName("test");
-        user.setPassword("1234");
+//        User user = new User();
+//        user.setId("1");
+//        user.setName("test");
+//        user.setPassword("1234");
+//
+//        userDao.add(user);
+//
+//        System.out.println("登録完了");
+//
+//        User getUser = userDao.get(user.getId());
+//
+//        System.out.println("getUser.getId() = " + getUser.getId());
+//        System.out.println("getUser.getName() = " + getUser.getName());
+//        System.out.println("getUser.getPassword() = " + getUser.getPassword());
 
-        userDao.add(user);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao1 = daoFactory.userDao();
+        UserDao userDao2 = daoFactory.userDao();
 
-        System.out.println("登録完了");
+        System.out.println("userDao1 = " + userDao1);
+        System.out.println("userDao2 = " + userDao2);
 
-        User getUser = userDao.get(user.getId());
+        UserDao userDao3 = applicationContext.getBean("userDao", UserDao.class);
+        UserDao userDao4 = applicationContext.getBean("userDao", UserDao.class);
 
-        System.out.println("getUser.getId() = " + getUser.getId());
-        System.out.println("getUser.getName() = " + getUser.getName());
-        System.out.println("getUser.getPassword() = " + getUser.getPassword());
+        System.out.println("userDao3 = " + userDao3);
+        System.out.println("userDao4 = " + userDao4);
     }
 }
