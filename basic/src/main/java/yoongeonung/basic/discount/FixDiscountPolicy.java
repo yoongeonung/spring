bd0612352f0b93e491b3cfca8a8414965986b78f
yoongeonung <yoongeonung@gmail.com>
2022-04-21 07:50:28 +0900

    feat : BeanName matching & @Qualifier & @Primary

--- basic/src/main/java/yoongeonung/basic/discount/FixDiscountPolicy.java
+++ basic/src/main/java/yoongeonung/basic/discount/FixDiscountPolicy.java
@@ -1,8 +1,12 @@
 package yoongeonung.basic.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import yoongeonung.basic.member.Grade;
 import yoongeonung.basic.member.Member;
 
+@Component
+@Qualifier("subDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private static final int FIX_DISCOUNT_AMOUNT = 1000;
--- basic/src/main/java/yoongeonung/basic/discount/RateDiscountPolicy.java
+++ basic/src/main/java/yoongeonung/basic/discount/RateDiscountPolicy.java
@@ -1,10 +1,13 @@
 package yoongeonung.basic.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import yoongeonung.basic.member.Grade;
 import yoongeonung.basic.member.Member;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private static final int DISCOUNT_RATE = 10;
