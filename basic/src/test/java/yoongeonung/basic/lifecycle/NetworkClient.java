760e299dda94cc1c92d7fd5ea239c3c4a1267d80
yoongeonung <yoongeonung@gmail.com>
2022-04-25 20:08:22 +0900

    feat : @Bean(initMethod, destroyMetehod)

--- basic/src/test/java/yoongeonung/basic/lifecycle/NetworkClient.java
+++ basic/src/test/java/yoongeonung/basic/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package yoongeonung.basic.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,15 +27,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
+        call("초기화 연결 메세지");
     }
 }
