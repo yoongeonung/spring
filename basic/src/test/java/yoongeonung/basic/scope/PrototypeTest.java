bc5e10b512da76bfc605006e20a9e73f456354c8
yoongeonung <yoongeonung@gmail.com>
2022-04-25 21:40:28 +0900

    feat : PrototypeBean

--- basic/src/test/java/yoongeonung/basic/scope/PrototypeTest.java
+++ basic/src/test/java/yoongeonung/basic/scope/PrototypeTest.java
@@ -1,4 +1,42 @@
 package yoongeonung.basic.scope;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PrototypeTest {
+    
+    @Test
+    void prototypeBeanFind() {
+        //given
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        //when
+        applicationContext.close();
+        //then
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("PrototypeBean.destory");
+        }
+
+    }
 }
