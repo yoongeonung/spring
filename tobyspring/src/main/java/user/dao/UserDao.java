69fa83144ab536d8d8e9ff6c442e93aa7626b3b5
yoongeonung <yoongeonung@gmail.com>
2022-04-29 21:47:43 +0900

    toby - @Befroe, TDD, test code refactor

--- tobyspring/src/main/java/user/dao/UserDao.java
+++ tobyspring/src/main/java/user/dao/UserDao.java
@@ -1,80 +1,91 @@
 package user.dao;
 
-import user.domain.User;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
-import java.sql.*;
+import org.springframework.dao.EmptyResultDataAccessException;
+import user.domain.User;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
-    private DataSource dataSource;
+  private ConnectionMaker connectionMaker;
+  private DataSource dataSource;
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+  public void setConnectionMaker(ConnectionMaker connectionMaker) {
+    this.connectionMaker = connectionMaker;
+  }
+
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public void add(User user) throws SQLException, ClassNotFoundException {
+    Connection connection = dataSource.getConnection();
+
+    PreparedStatement preparedStatement = connection.prepareStatement(
+        "insert into users(id,name,password) values (?,?,?)");
+    preparedStatement.setString(1, user.getId());
+    preparedStatement.setString(2, user.getName());
+    preparedStatement.setString(3, user.getPassword());
+
+    preparedStatement.executeUpdate();
+
+    preparedStatement.close();
+    connection.close();
+  }
+
+  public User get(String id) throws SQLException {
+    Connection connection = dataSource.getConnection();
+
+    PreparedStatement preparedStatement = connection.prepareStatement(
+        "select * from users where id = ?");
+    preparedStatement.setString(1, id);
+
+    ResultSet resultSet = preparedStatement.executeQuery();
+
+    User user = null;
+    if (resultSet.next()) {
+      user = new User();
+      user.setId(resultSet.getString("id"));
+      user.setName(resultSet.getString("name"));
+      user.setPassword(resultSet.getString("password"));
     }
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    resultSet.close();
+    preparedStatement.close();
+    connection.close();
+
+    if (user == null) {
+      throw new EmptyResultDataAccessException(1);
     }
 
-    public void add(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = dataSource.getConnection();
+    return user;
+  }
 
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id,name,password) values (?,?,?)");
-        preparedStatement.setString(1, user.getId());
-        preparedStatement.setString(2, user.getName());
-        preparedStatement.setString(3, user.getPassword());
+  public void deleteAll() throws SQLException {
+    Connection connection = dataSource.getConnection();
 
-        preparedStatement.executeUpdate();
+    PreparedStatement ps = connection.prepareStatement("delete from users");
+    ps.executeUpdate();
 
-        preparedStatement.close();
-        connection.close();
-    }
+    ps.close();
+    connection.close();
+  }
 
-    public User get(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = dataSource.getConnection();
+  public int getCount() throws SQLException {
+    Connection connection = dataSource.getConnection();
+    PreparedStatement ps = connection.prepareStatement("select count(*) from users");
+    ResultSet rs = ps.executeQuery();
+    rs.next();
+    int count = rs.getInt(1); // index가 1부터 시작
 
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
-        preparedStatement.setString(1, id);
+    rs.close();
+    ps.close();
+    connection.close();
 
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-
-        User user = new User();
-        user.setId(resultSet.getString("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-
-        return user;
-    }
-
-    public void deleteAll() throws SQLException {
-        Connection connection = dataSource.getConnection();
-
-        PreparedStatement ps = connection.prepareStatement("delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        connection.close();
-    }
-
-    public int getCount() throws SQLException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement ps = connection.prepareStatement("select count(*) from users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1); // index가 1부터 시작
-
-        rs.close();
-        ps.close();
-        connection.close();
-
-        return count;
-    }
+    return count;
+  }
 
 }
--- tobyspring/src/main/java/user/dao/UserDaoTest.java
+++ tobyspring/src/main/java/user/dao/UserDaoTest.java
@@ -5,33 +5,71 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.sql.SQLException;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import user.domain.User;
 
 public class UserDaoTest {
 
-  @Test
-  public void addAndGet() throws SQLException, ClassNotFoundException {
+  private UserDao userDao;
+
+  @Before
+  public void setUp() {
     ApplicationContext applicationContext = new GenericXmlApplicationContext(
         "applicationContext.xml");
-    UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
+    this.userDao = applicationContext.getBean("userDao", UserDao.class);
+  }
+
+  @Test
+  public void addAndGet() throws SQLException, ClassNotFoundException {
+    User user1 = new User("1", "userA", "1234");
+    User user2 = new User("2", "userB", "5678");
+
     userDao.deleteAll();
+    assertThat(userDao.getCount(), is(0));
 
-    int count = userDao.getCount();
-    assertThat(count, is(0));
+    userDao.add(user1);
+    userDao.add(user2);
+    assertThat(userDao.getCount(), is(2));
 
+    User getUser1 = userDao.get(user1.getId());
+    assertThat(getUser1.getName(), is(user1.getName()));
+    assertThat(getUser1.getPassword(), is(user1.getPassword()));
 
-    User user = new User("1", "userA", "1234");
-    userDao.add(user);
+    User getUser2 = userDao.get(user2.getId());
+    assertThat(getUser2.getName(), is(user2.getName()));
+    assertThat(getUser2.getPassword(), is(user2.getPassword()));
+  }
+
+  @Test
+  public void count() throws SQLException, ClassNotFoundException {
+    User user1 = new User("1", "userA", "1111");
+    User user2 = new User("2", "userB", "2222");
+    User user3 = new User("3", "userC", "3333");
+    // OPERATE
+    // CHECK
+    userDao.deleteAll();
+    assertThat(userDao.getCount(), is(0));
+
+    userDao.add(user1);
     assertThat(userDao.getCount(), is(1));
 
-    User findUser = userDao.get(user.getId());
+    userDao.add(user2);
+    assertThat(userDao.getCount(), is(2));
 
-    assertThat(findUser.getName(), is(user.getName()));
-    assertThat(findUser.getPassword(), is(user.getPassword()));
-    assertThat(findUser.getId(), is(user.getId()));
+    userDao.add(user3);
+    assertThat(userDao.getCount(), is(3));
+  }
+
+  @Test(expected = EmptyResultDataAccessException.class)
+  public void getUserFailure() throws SQLException {
+    userDao.deleteAll();
+    assertThat(userDao.getCount(), is(0));
+
+    userDao.get("unknown_id"); // 예외 발생
   }
 
 }
