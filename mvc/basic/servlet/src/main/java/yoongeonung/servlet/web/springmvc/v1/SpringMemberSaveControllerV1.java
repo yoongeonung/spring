688c421792036c2a92972af3350e70fdedca08ab
yoongeonung <yoongeonung@gmail.com>
2022-05-16 21:18:41 +0900

    MVC : @Controller, @RequestMapping, ModelAndView

---
+++ mvc/basic/servlet/src/main/java/yoongeonung/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,28 @@
+package yoongeonung.servlet.web.springmvc.v1;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import yoongeonung.servlet.domain.member.Member;
+import yoongeonung.servlet.domain.member.MemberRepository;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+  private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @RequestMapping("/springmvc/v1/members/save")
+  public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    String username = request.getParameter("username");
+    int age = Integer.parseInt(request.getParameter("age"));
+
+    Member member = new Member(username, age);
+    memberRepository.save(member);
+
+    ModelAndView mv = new ModelAndView("save-result");
+    mv.addObject("member", member);
+    return mv;
+  }
+}
