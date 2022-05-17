defbd19d0275bc2f2b901a68e68171d62d189408
yoongeonung <yoongeonung@gmail.com>
2022-05-18 08:34:30 +0900

    MVC : @RequestMapping

---
+++ mvc/basic/springmvc/src/test/java/yoongeonung/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package yoongeonung.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
