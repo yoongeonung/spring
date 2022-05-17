fd42822263cc6f75c013fe976a1e14e1659515e4
yoongeonung <yoongeonung@gmail.com>
2022-05-17 21:19:36 +0900

    TOBY : First Chap

--- tobyspring/src/main/java/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
