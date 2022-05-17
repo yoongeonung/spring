34f2c48fa95e8229c0aeecd063d06047d3e6c305
yoongeonung <yoongeonung@gmail.com>
2022-05-17 21:38:09 +0900

    TOBY : User & UserDao

---
+++ tobyspring/src/main/java/user/domain/User.java
@@ -0,0 +1,11 @@
+package user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+  private String id;
+  private String name;
+  private String password;
+}
